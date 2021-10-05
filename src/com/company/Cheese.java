a5c9f3efbbb0e429e5ae361bce42d77a203f324e
Kazybek03 <77659303+Kazybek03@users.noreply.github.com>
2021-10-05 22:09:15 +0600

    Add files via upload

---
+++ src/com/company/Cheese.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Cheese extends ToppingDecorator {
+
+    public Cheese(Pizza pizza) {
+        this.pizza = pizza;
+    }
+
+    public String getDescription() {
+        return pizza.getDescription() + ", Cheese";
+    }
+
+    public double cost() {
+        return pizza.cost();
+    }
+}
---
+++ src/com/company/ThincrustPizza.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class ThincrustPizza extends Pizza {
+    public ThincrustPizza() {
+        description = "Thin crust pizza, with tomato sauce";
+    }
+
+    public double cost() {
+        return 7.99;
+    }
+}
